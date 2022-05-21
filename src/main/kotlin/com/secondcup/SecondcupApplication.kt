05959013ff9fcb2b0747d69ba48a9caaf792de29
jongseok <jongseok@tmon.co.kr>
2022-05-21 16:11:45 +0900

    initial commit

---
+++ src/main/kotlin/com/secondcup/SecondcupApplication.kt
@@ -0,0 +1,11 @@
+package com.secondcup
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecondcupApplication
+
+fun main(args: Array<String>) {
+	runApplication<SecondcupApplication>(*args)
+}
---
+++ src/test/kotlin/com/secondcup/SecondcupApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.secondcup
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecondcupApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
