838433f1c31dc363214e47bf8d17c6470545dae4
jongseok <jongseok@tmon.co.kr>
2022-05-21 23:08:26 +0900

    h2로 jpa 테스트 가능한 상태

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,18 +16,27 @@ repositories {
 	mavenCentral()
 }
 
+allOpen {
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.MappedSuperclass")
+	annotation("javax.persistence.Embeddable")
+}
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.apache.commons:commons-lang3")
 	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+	providedRuntime("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
+		jvmTarget = "1.8"
 	}
 }
 
